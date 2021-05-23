9b63c0de50d6979c7dcd833a0bc562c3bb537e41
Khalid Muhammad <khalidmuhammad1100@gmail.com>
2021-05-23 20:21:51 +0700

    Set database pool settings from env

--- models/setup.go
+++ models/setup.go
@@ -3,6 +3,8 @@ package models
 import (
 	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -16,6 +18,26 @@ func ConnectDatabase() {
 	if err != nil {
 		log.Fatalf("Error connecting to database: %v", err)
 	}
+
+	sqlDB, _ := database.DB()
+
+	maxIdleConns, err := strconv.Atoi(os.Getenv("MAX_IDLE_CONNECTIONS"))
+	if err != nil {
+		log.Fatalf("MAX_IDLE_CONNECTIONS should be an integer, given %v", os.Getenv("MAX_IDLE_CONNECTIONS"))
+	}
+	sqlDB.SetMaxIdleConns(maxIdleConns)
+
+	maxOpenConns, err := strconv.Atoi(os.Getenv("MAX_OPEN_CONNECTIONS"))
+	if err != nil {
+		log.Fatalf("MAX_OPEN_CONNECTIONS should be an integer, given %v", os.Getenv("MAX_OPEN_CONNECTIONS"))
+	}
+	sqlDB.SetMaxOpenConns(maxOpenConns)
+
+	connMaxLifetime, err := strconv.Atoi(os.Getenv("CONNECTION_MAX_LIFETIME_MINUTES"))
+	if err != nil {
+		log.Fatalf("CONNECTION_MAX_LIFETIME_MINUTES should be an integer, given %v", os.Getenv("CONNECTION_MAX_LIFETIME_MINUTES"))
+	}
+	sqlDB.SetConnMaxLifetime(time.Duration(connMaxLifetime) * time.Minute)
 
 	database.AutoMigrate(&User{})
 	database.AutoMigrate(&Tweet{})
